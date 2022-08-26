d678fb4915fd8d4c102dd382ef8581e9a8f494a1
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2022-08-26 18:01:24 +0530

    commit

---
+++ binary trees/dfs.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+
+    node(int val){
+        data = val;
+        left=right=NULL;
+    }
+};
+
+//in-order traversal
+void inorder(struct node* root){
+    if(root!= NULL){
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+
+//pre-order traversal
+void preorder(struct node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// post-order traversal
+void postorder(struct node* root){
+     if(root==NULL){
+        return;
+     }
+     postorder(root->left);
+     postorder(root->right);
+     cout<<root->data<<" ";
+}
+
+int main(){
+    struct node * root = new node(1);
+
+    root->left = new node(2);
+    root->right = new node(3);
+    root-> left->right = new node(5);
+    inorder(root);
+    preorder(root);
+    postorder(root);
+}
--- binary trees/tree_implementation.cpp
+++ binary trees/tree_implementation.cpp
@@ -12,10 +12,12 @@ struct node{
     }
 };
 
+
 int main(){
     struct node * root = new node(1);
 
     root->left = new node(2);
     root->right = new node(3);
     root-> left->right = new node(5);
+    
 }
