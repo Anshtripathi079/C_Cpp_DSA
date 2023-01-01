7b33782aaa000ef96a87f632b7123fae89cd809c
Ansh Tripathi <110754826+Anshtripathi079@users.noreply.github.com>
2023-01-01 18:57:52 +0530

    Added question

---
+++ recurssion/reverse_array_using_recurssion.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reversearr(int arr[],int i,int j){
+    if(i>j){
+        return;
+    }
+    swap(arr[i],arr[j]);
+    return reversearr(arr,i+1,j-1);
+}
+void printarr(int arr[]){
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int i = 0;
+    int j = 4;
+    printarr(arr);
+    reversearr(arr,i,j);
+    printarr(arr);
+}
